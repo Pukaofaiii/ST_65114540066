f9998b79ba1e9988f9c3bf9a005a1fc8d8f7d2d3
piixy <108978921+Pukaofaiii@users.noreply.github.com>
2025-07-02 16:21:27 +0700

    Delete djangoPJ directory

--- djangoPJ/mysite/book/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("ยินดีต้อนรับสู่ Book App!")
--- djangoPJ/symite/product/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.landing_page, name='landing_page'),
-    path('', views.product_list, name='product_list'),
-    
-]
